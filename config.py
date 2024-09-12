ef4d5a4ec04d1e53919662bd948bcacecc4eb53a
Rahul700-sudo <Rahul700@outlook.in>
2024-09-12 19:13:13 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY SASUKE
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7489795692:AAFl-8d1S5Lf-WgH9skDbxFirj_xm6F4TOM")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@Mr_sid_001")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1329951770")
+
+
+
+
+
+
+
+  
